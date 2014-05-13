ac9d426235405fa74f2b79208a32badc5c5e7d8a
jking <jking@cj.com>
2014-05-12 22:26:50 -0700

    what are the odds

---
+++ odds/beerOdds.py
@@ -0,0 +1,7 @@
+# what are the odds of two people choosing four distinct numbers out of 
+# 103 numbers, and their two selections having an overlap of two numbers?
+
+print "hello"
+
+# monte carlo approach
+
