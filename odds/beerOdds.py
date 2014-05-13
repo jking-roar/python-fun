1afb815afb5fae565e27ab70b769c2a933938dbb
jking <jking@cj.com>
2014-05-13 07:31:32 -0700

    clean up output

--- odds/beerOdds.py
+++ odds/beerOdds.py
@@ -1,4 +1,4 @@
-# what are the odds of two people choosing four distinct numbers out of 
+# what are the odds of two people choosing four distinct numbers out of
 # 103 numbers, and their two selections having an overlap of two numbers?
 from random import sample
 
@@ -14,16 +14,24 @@ print "what are the odds that {0} beers overlap?".format(overlap)
 print
 
 # monte carlo approach
-total_attempts = 200000
+total_attempts = 1000000
+print "making {0} beer selection simulations".format(total_attempts)
+print
 
 choices = range(1, numBeers + 1)
 numMatchRequirements = 0
-for attempt in range(0, total_attempts): 
+for attempt in range(0, total_attempts):
     selections = [sample(choices, selectNum) for person in range(0, numPeople)]
     matches = sum([all([(firstSelection in y) for y in selections[1:]]) for firstSelection in selections[0]])
     if(matches == overlap):
       numMatchRequirements += 1
 
-
-print "there were {0} out of {1} samples where {2} beers were overlapping with all the selections".format(numMatchRequirements, total_attempts, overlap)
-print "that is {0:0.3f}%".format(numMatchRequirements * 100.0 / total_attempts)
+print
+print "according to my simulations:"
+print "  {0} out of {1} selections had {2} beers overlapping".format(numMatchRequirements, total_attempts, overlap)
+probability = numMatchRequirements * 1.0 / total_attempts
+percent = probability * 100
+print "  percent is {0:0.3f}%".format(percent)
+against = 1 - probability
+odds = against / probability
+print "  odds are ~ {0:0.3f} to one against".format(odds)
