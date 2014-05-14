66e200fe79e813ca1b00b4729ff74175887deef0
jking <jking@cj.com>
2014-05-13 23:21:36 -0700

    math applied

--- odds/beerOdds.py
+++ odds/beerOdds.py
@@ -11,6 +11,30 @@ print "there are {0} people.".format(numPeople)
 print "they all make a selection of {0} beers".format(selectNum)
 print "what are the odds that {0} beers overlap?".format(overlap)
 
+top = 1.0;
+bottom = 1.0;
+def f(a):
+    if(a < 2):
+        return 1
+    else:
+        return a * f(a-1)
+def c(fro, ch):
+    a = f(fro) / (f(ch) * f(fro - ch))
+    return a
+
+top *= c(selectNum, overlap)
+top *= c(numBeers - selectNum, selectNum - overlap)
+bottom *= c(numBeers, selectNum)
+
+probability = top * 1.0/bottom
+percent = probability * 100
+print
+print "according to combinatoric calculations:"
+print "  probability is {0} over {1}, or {2:0.09f}%".format(top, bottom, percent)
+against = 1 - probability
+odds = against / probability
+print "  odds are {0:0.3f} to one against".format(odds)
+
 print
 
 # monte carlo approach
